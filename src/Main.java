4156f1d568a93498a72f59694bd59a5c8c7f5045
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-05-21 19:17:17 +0400

    third commit

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,22 @@ public class Main {
 
         int startBalanceInKopecks = 100_00; //в копейках
         int refillInKopecks = 1000_00; //в копейках
-        int refillClientInKopecks = 1100_00; //в копейках
+        int refillClientInKopecks = 100_00; //в копейках
         int startBonus = 100_00; //в копейках
         int bonusInKopecks = 0;
         int totalBalance;
 
         if (refillClientInKopecks > refillInKopecks) {
             bonusInKopecks = (refillClientInKopecks / startBonus) * 100; //в копейках
-            System.out.println("Вы пополнили баланс на " + refillClientInKopecks / 100 + " руб и получили " + bonusInKopecks / 100 + " бонусов");
+            System.out.println("Вы получили " + bonusInKopecks / 100 + " бонусов");
 
         } else {
-            System.out.println("Вы пополнили баланс на " + refillClientInKopecks / 100 + " руб. Бонусов нет");
+            System.out.println("Бонусов нет");
 
         }
         totalBalance = startBalanceInKopecks + refillClientInKopecks + bonusInKopecks;
-        System.out.println("Итоговая сумма на счете: " + totalBalance / 100);
+        System.out.println("Вы пополнили баланс на " + refillClientInKopecks / 100 + " руб");
+        System.out.println("Итоговая сумма на счете: " + totalBalance / 100 + " руб");
 
     }
 }
